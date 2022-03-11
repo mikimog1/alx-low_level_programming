95c63c07dd24f51ed67ff8228ddc201597c91a87
mikimog1 <mikimog23@gmail.com>
2022-03-11 10:32:22 -0800

    loop and if

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
 
-/**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
- */
 
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+  int afirst;
+  int a;
+  int bfirst;
+  int b;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
-
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
-
-	return (0);
+  for(afirst=48; afirst<58; afirst++)
+    {
+      for (a=48; a<58; a++)
+        {
+        b=a+1;
+      bfirst=afirst;
+      for (; bfirst<58; bfirst++)
+        {
+          for (; b<58; b++)
+  {
+              putchar(afirst);
+              putchar(a);
+              putchar(' ');
+              putchar(bfirst);
+              putchar(b);
+              if (afirst != 57 || bfirst != 57 ||a != 56 || b != 57)
+                {
+                  putchar(',');
+                  putchar(' ');
+                }
+            }
+          b=48;
+        }
+      }
+    }
+  putchar('\n');
+  return (0);
 }
