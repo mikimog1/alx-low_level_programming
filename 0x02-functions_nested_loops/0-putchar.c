481068d6c85adc9ab28d81ce4ddc99add6b1fae3
mikimog1 <mikimog23@gmail.com>
2022-03-15 11:36:54 -0700

    nasted loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-/*
- *this code prints _putchar
- *
- */
-int main()
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
+int main(void)
 {
-  char *write = "_putchar";
+	char prt[] = "_putchar";
 
-  while(*write)
-{
-  putchar(*write);
-  write++;
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(prt[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
-  putchar('\n');
-  return 0;
-}
-	
